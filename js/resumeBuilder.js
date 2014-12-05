ba9860490f6f557066165d931d3b8be824a02eaa
Olu <olumayowa.abodunrin@adfero.co.uk>
2014-12-05 13:15:51 +0000

    Added bio information to the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,51 @@
-var name = "Olu Plantain";
-var role = "Chosen One";
+var bio = {
+	name : "Olu Plantain",
+	role : "Person",
+	contactInfo : {
+		mobile : "678998212",
+		email : "test@aol.com",
+		twitter : "@waa",
+		github : "Mayoweezy",
+		blog : "don't have one",
+		location : "somewhere",
+		generic : "generic info" 
+	},
+	pictureURL : "images/fry.jpg",
+	skills : ["football manager on fm15", "software developer", "sunday league level footballer"]
+}
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
-var skills = ["tracksuit", ""];
+var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+var formattedBlog = HTMLblog.replace("%data%", bio.contactInfo.blog);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+//var formattedContactGeneric = HTMLcontactGeneric.replace("%data%", bio.contactInfo.generic);
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+
+$("#header").append(HTMLskillsStart);
+for (var i = 0; i < bio.skills.length; i++) {	
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+	$("#skills").append(formattedSkill);
+};
+
+
+
+// appending header information to page
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
+
+
+// appending contact information to page
+$("#topContacts").prepend(formattedEmail);
+$("#footerContacts").append(formattedMobile);
+$("#footerContacts").append(formattedBlog);
+$("#footerContacts").append(formattedGithub);
+$("#footerContacts").append(formattedTwitter);
+$("#footerContacts").append(formattedLocation);
+//$("#topContacts").append(formattedContactGeneric);
